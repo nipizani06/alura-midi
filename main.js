a7ae3a0ef2548ec47d3c3b7a2f83ea2e9409dad8
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2023-09-20 14:26:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 function tocaSomPom (seletorAudio) {
-document.querySelector(idElementoAudio).play();
+ const elemento = document.querySelector(seletorAudio).play();
 }
 
+if (elemento && elemento.localName === 'audio') {
+  elemento.play();
+ }
+ else {
+  //alert('Elemento não encontrado');
+  console.log('Elemento não encontrado ou seletor inválido');
+ }
+
+}
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
